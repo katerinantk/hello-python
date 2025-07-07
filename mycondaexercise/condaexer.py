2d729eac848944a9ce8012786a8e84148e65ab88
Katerina Ntouka <katerinantouka@MacBook-Air-Katerina.local>
2025-07-07 12:13:09 +0300

    Initial commit

---
+++ mycondaexercise/condaexer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+x = a + b
+
+print(x)
+
+import matplotlib.pyplot as plt 
+
+c = [1, 2, 3]
+d = [3, 5, 7]
+
+plt.plot(c, d)
+plt.show()
